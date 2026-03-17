851206e221d5323c77815dabf3c8a839c4ba895f
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-03-17 16:12:57 +0200

    Added the StrapiResponse and StrapiProject types in the types.ts file

--- app/types.ts
+++ app/types.ts
@@ -1,5 +1,6 @@
 export type Project = {
   id: string;
+  documentId: string;
   title: string;
   description: string;
   image: string;
@@ -15,4 +16,28 @@ export type PostMeta = {
   title: string;
   excerpt: string;
   date: string;
+};
+
+export type StrapiResponse<T> = {
+  data: T[];
+};
+
+export type StrapiProject = {
+  id: string;
+  documentId: string;
+  title: string;
+  description: string;
+  image?: {
+    url: string;
+    formats?: {
+      thumbnail?: { url: string };
+      small?: { url: string };
+      medium?: { url: string };
+      large?: { url: string };
+    };
+  };
+  url: string;
+  date: string;
+  category: string;
+  featured: boolean;
 };
